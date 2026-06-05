7ad637feaf35d42b4b115883d8b3a1d73691242c
sjshb57 <1290848232a@gmail.com>
2026-06-05 10:26:35 +0800

    Enable release minification and update workflow push command

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
-            isShrinkResources = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             signingConfig = signingConfigs.getByName("debug")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
